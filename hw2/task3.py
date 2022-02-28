23b743ccc599ac55a55ba014f1dce1b46b7018aa
senya505 <semenmek@gmail.com>
2022-02-28 12:56:48 +0300

    Remove import

--- hw2/task3.py
+++ hw2/task3.py
@@ -1,9 +1,6 @@
-from curses.ascii import isdigit, islower, isupper
-
-
 def is_password_secure(password: str) -> bool:
-    return any(islower(c) for c in password) and any(isupper(c) for c in password) \
-        and any(isdigit(c) for c in password) and len(password) >= 8
+    return any(c.islower() for c in password) and any(c.isupper() for c in password) \
+        and any(c.isdigit() for c in password) and len(password) >= 8
 
 if __name__ == '__main__':
     password = input('Введите пароль: ')
